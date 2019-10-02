8e6c64912511ffab692e209f93bfe0f43389cb25
kenkiti <kenkiti@gmail.com>
2019-10-02 15:13:38 +0900

    initial commit

--- testOptions/Form1.cs
+++ testOptions/Form1.cs
@@ -8,13 +8,59 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using CommandLine;
+
 namespace testOptions
 {
     public partial class Form1 : Form
     {
+        private Options opt = null;
+
         public Form1()
         {
             InitializeComponent();
+
+            string[] args = System.Environment.GetCommandLineArgs();
+
+            // ジェネリクスでオプションクラスを指定し、パースする
+            var parseResult = Parser.Default.ParseArguments<Options>(args);
+
+            // 結果はTagに入っている
+            switch (parseResult.Tag)
+            {
+                // パース成功
+                case ParserResultType.Parsed:
+                    // パースの成否でパース結果のオブジェクトの方が変わる
+                    var parsed = parseResult as Parsed<Options>;
+
+                    // 成功時はキャストしたオブジェクトからパース結果が取得可能
+                    opt = parsed.Value;
+
+                    Console.WriteLine($"opt.code = {opt.code}");
+                    Console.WriteLine($"opt.start_auto = {opt.start_auto}");
+                    Console.WriteLine($"opt.connect = {opt.connect_server}");
+
+                    break;
+                // パース失敗
+                case ParserResultType.NotParsed:
+                    // パースの成否でパース結果のオブジェクトの方が変わる
+                    var notParsed = parseResult as NotParsed<Options>;
+
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string[] Commands = System.Environment.GetCommandLineArgs();
+
+            for (int i = 0; i < Commands.Length; i++)
+            {
+                textBox1.Text += string.Format("command : {0:d} : {1}\r\n", i, Commands[i]);
+            }
+
+
         }
     }
 }
---
+++ testOptions/Options.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommandLine;
+
+namespace testOptions
+{
+
+
+    class Options
+    {
+        [Option('c', "code", Default = 4565, HelpText = "code")]
+        public int code { get; set; }
+
+        [Option('s', "start", Required = false, HelpText = "autostart")]
+        public bool start_auto { get; set; }
+
+        [Option('o', "connect", Required = false, HelpText = "connect server")]
+        public bool connect_server { get; set; }
+
+    }
+}
